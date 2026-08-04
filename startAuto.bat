57eb12728e3e35933b95b0a8653e38bac26589cd
danimit1254663 <razkindanil@gmail.com>
2026-08-05 01:36:43 +0500

    Assitent(Jarvis) v1

--- startAuto.bat
+++ startAuto.bat
@@ -1,6 +1,22 @@
 @echo off
+cd /d "C:\Users\Admin\Desktop\AI-Portal-2-main"
 
-cd /d %~dp0
+echo Creating virtual environment...
+
+if not exist venv (
+    py -3.11 -m venv venv
+)
+
+echo Activating environment...
+
+call venv\Scripts\activate.bat
+
+echo Installing requirements...
+
+python -m pip install -r requirements.txt
+
+echo Starting AI Portal...
 
 python main.py
+
 pause
